b007f6e3ed72ba5a5e3af5703c0602658f739e2f
Nick Fisher <nick.fisher@avinium.com>
2019-06-24 09:22:30 +1000

    update header

--- ios/Classes/InmobiPlugin.h
+++ ios/Classes/InmobiPlugin.h
@@ -3,4 +3,7 @@
 @interface InmobiPlugin : NSObject<FlutterPlugin,IMInterstitialDelegate>
 @property NSString* accountId;
 @property (nonatomic, strong) IMInterstitial *interstitial;
+
+- (void)loadInterstitial;
+- (void)showInterstitial;
 @end
